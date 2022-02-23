a2aae2b2476bbecdaa1a4cf774e2f7c6beb4c4c6
Hwangwonuk <hok0173@naver.com>
2022-02-23 15:35:10 +0900

    useNotification 이동

--- src/hooks.js
+++ src/hooks.js
@@ -1,26 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
-const useNotification = (title, options) => {
-    if (!("Notification" in window)) {
-        return;
-    }
-    const fireNotif = () => {
-        if (Notification.permission !== "granted") {
-            Notification.requestPermission().then(permission => {
-                if (permission === "granted") {
-                    new Notification(title, options);
-                } else {
-                    return;
-                }
-            });
-        } else {
-            new Notification(title, options);
-        }
-    };
-    return fireNotif;
-}
-
 const App = () => {
     const trigerNotifi = useNotification("Can I steal your kimchi?", {
         body: "I love kimchi dont you"
