cb7df2140c323323bc614526e87e4c34f918c1ab
Hwangwonuk <hok0173@naver.com>
2023-08-27 22:26:16 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import {
+  BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h2>Hello</h2>}></Route>
+        <Route path = "/" element={<Home />}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
-// commit
