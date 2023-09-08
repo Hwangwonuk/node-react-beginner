7135f05208c2a62dc77af2025666d09c9489b21a
Hwangwonuk <hok0173@naver.com>
2023-09-08 23:15:28 +0900

    useInput Part Three

--- src/hooks.js
+++ src/hooks.js
@@ -8,13 +8,20 @@ const useInput = (initialValue, validator) => {
         const {
             target: { value }
         } = event;
-        setValue(value);
+        let willUpdate = true;
+        if(typeof validator === "function") {
+            willUpdate = validator(value);
+        }
+        if (willUpdate) {
+            setValue(value);
+        }
     }
     return { value, onChange };
 }
 
 const App = () => {
-    const name = useInput("Mr.");
+    const maxLen = (value) => !value.includes("@");
+    const name = useInput("Mr.", maxLen);
     return (
         <div className="App">
             <h1>Hello</h1>
