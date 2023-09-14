49ed8a39aa3ff05fece15a5adab1a49ae82acb5c
Hwangwonuk <hok0173@naver.com>
2023-09-14 11:10:33 +0900

    useClick

--- src/hooks.js
+++ src/hooks.js
@@ -1,13 +1,30 @@
 import React, { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
+const useClick = (onClick) => {
+    if (typeof onClick !== "function") {
+        return;
+    }
+    const element = useRef();
+    useEffect(() => {
+        if (element.current) {
+            element.current.addEventListener("click", onClick);
+        }
+        return () => {
+            if (element.current) {
+                element.current.removeEventListener("click", onClick);
+            }
+        };
+    }, []);
+    return element;
+};
+
 const App = () => {
-    const potato = useRef();
-    setTimeout(() => potato.current.focus(), 5000);
+    const sayHello = () => console.log("say hello");
+    const title = useClick(sayHello);
     return (
         <div className="App">
-            <div>Hi</div>
-            <input ref={potato} placeholder="la" />
+            <h1 ref={title}>Hi</h1>
         </div>
     );
 };
